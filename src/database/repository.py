90cb2798ee2e4fd361d06bb60b90a7eb5498a389
James Patrick <jp1252211@gmail.com>
2026-07-12 00:59:19 +0300

    modified repo.py with bulk inserts

--- src/database/repository.py
+++ src/database/repository.py
@@ -1,10 +1,5 @@
 """
 Repository layer.
-
-All database interactions are centralized here.
-
-The repository hides SQLAlchemy implementation details from the rest
-of the application.
 """
 
 from datetime import datetime
@@ -17,50 +12,51 @@ from database.models import Job
 
 
 class JobRepository:
-    """
-    Repository responsible for persisting jobs.
-    """
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.session = SessionLocal()
 
-    def save(self, job: dict[str, Any]) -> bool:
+    def save_many(
+        self,
+        jobs: list[dict[str, Any]],
+    ) -> int:
         """
-        Insert a job into PostgreSQL.
+        Bulk insert jobs.
 
-        Duplicate job URLs are ignored using PostgreSQL's
-        ON CONFLICT DO NOTHING.
-
-        Returns
-        -------
-        bool
-            True if inserted.
-            False if skipped.
+        Returns number of inserted rows.
         """
+
+        values = []
+
+        for job in jobs:
+
+            values.append(
+                {
+                    "title": job["title"],
+                    "company": job["company"],
+                    "location": job["location"],
+                    "job_type": job.get("job_type"),
+                    "salary": job.get("salary"),
+                    "description": job.get("description"),
+                    "job_url": job["job_url"],
+                    "scraped_at": datetime.now(),
+                }
+            )
 
         statement = (
             insert(Job)
-            .values(
-                title=job["title"],
-                company=job["company"],
-                location=job["location"],
-                job_type=job.get("job_type"),
-                salary=job.get("salary"),
-                description=job.get("description"),
-                job_url=job["job_url"],
-                scraped_at=datetime.now(),
-            )
+            .values(values)
             .on_conflict_do_nothing(
                 index_elements=["job_url"]
             )
         )
 
         result = self.session.execute(statement)
+
         self.session.commit()
 
-        # PostgreSQL returns one affected row when inserted.
-        return result.rowcount == 1
+        return result.rowcount
 
-    def close(self) -> None:
-        """Close the database session."""
+    def close(self):
+
         self.session.close()
